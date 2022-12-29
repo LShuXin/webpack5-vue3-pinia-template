672442504f052330bc7e4788beaca1ee2e3b8427
LShuXin <1569663570@qq.com>
2022-12-29 21:51:39 +0800

    use pinia

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+const pinia = createPinia()
 const app = createApp(App)       // 创建vue实例
+app.use(pinia)
 app.use(router)                  // 使用路由
 app.mount('#app')                // 挂载到id为app的div
--- src/router/Home.vue
+++ src/router/Home.vue
@@ -1,15 +1,27 @@
 <template>
   <h1>Home{{ state.count }}</h1>
+  <h1>{{ counter.count }}</h1>
   <button @click="addRecord">点击</button>
   <div v-for="(item, idx) in state.msgList">{{ item.a + '' + item.b }}</div>
 </template>
 
 <script>
 import { reactive } from 'vue'
+import { useCounterStore } from '@/stores/counter'
 import db from '@/utils/db'
 export default {
   // `setup` 是一个专门用于组合式 API 的特殊钩子函数
   setup() {
+    const counter = useCounterStore()
+
+    counter.count++
+    // 带有自动补全 ✨
+    counter.$patch({ count: counter.count + 1 })
+    // 或者使用 action 代替
+    counter.increment()
+
+
+
     const state = reactive({
       count: 0,
       msgList: [ {'a': 1, b: 2}],
@@ -28,14 +40,9 @@ export default {
     }
 
     
-
-
-
     function increment() {
-      
       state.count++
     }
-
 
 
     // 不要忘记同时暴露 increment 函数
@@ -43,6 +50,7 @@ export default {
       state,
       increment,
       addRecord,
+      counter
     }
   }
 }
